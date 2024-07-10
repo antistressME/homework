de4d9d47b270854e2e7f06ac9fc6f65c1685d4e8
Anastasia <antipinaanastasia0112@gmail.com>
2024-07-10 18:24:07 +0300

    update import

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,4 @@
-from masks.py import get_mask_account
-from masks.py import get_mask_card_number
+from masks.py import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_card: str) -> str:
