393206de222424c598aba4d70ef7f5ba311c8cf3
Anastasia <antipinaanastasia0112@gmail.com>
2024-07-25 21:16:51 +0300

    ubdate test of error

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -11,8 +11,9 @@ def test_get_mask_card_number():
     assert get_mask_card_number("12341234") == "Введите номер карты полностью"
 
 
-with pytest.raises(TypeError):
-    get_mask_card_number(1234123412341234)
+def test_error_get_mask_card_number():
+    with pytest.raises(TypeError):
+        get_mask_card_number(1234123412341234)
 
 
 def test_get_mask_account():
@@ -20,6 +21,6 @@ def test_get_mask_account():
     assert get_mask_account("0123456789012345678") == "**5678"
     assert get_mask_account("01234567") == "**4567"
 
-
-with pytest.raises(TypeError):
-    get_mask_account(12341234123412341234)
+def test_error_get_mask_account():
+    with pytest.raises(TypeError):
+        get_mask_account(12341234123412341234)
