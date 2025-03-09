9924dcb55239963f19c317fd205f96054c30afb5
Anastasia <antipinaanastasia0112@gmail.com>
2025-03-09 09:23:34 +0300

    update filter_by_description func"

--- src/filters.py
+++ src/filters.py
@@ -18,11 +18,12 @@ if __name__ == "__main__":
     print(filter_by_str(operations, search_str))
 
 
-def filter_dy_description(operations: list[dict], description: list) -> dict:
+def filter_by_description(operations: list[dict], description: list) -> dict:
     """Получение соваря категорий и количества операций в каждой категории."""
     result = {}
     for item in description:
-        descriptions = re.findall(item, str(operations))
+        pattern = r'description\W\W\s\W' + item
+        descriptions = re.findall(pattern, str(operations))
         result[item] = len(descriptions)
     return result
 
@@ -30,4 +31,4 @@ def filter_dy_description(operations: list[dict], description: list) -> dict:
 if __name__ == "__main__":
     operations = get_list("operations.json")
     description = ["Перевод организации", "Перевод со счета на счет"]
-    print(filter_dy_description(operations, description))
+    print(filter_by_description(operations, description))
