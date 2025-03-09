7eba071b9a1eb895c2f25fa7b074ff84cfab409d
Anastasia <antipinaanastasia0112@gmail.com>
2025-03-09 10:18:43 +0300

    udate filters

--- src/filters.py
+++ src/filters.py
@@ -1,13 +1,15 @@
 import re
 
 from src.utils import get_list
-
+from collections import Counter
+from collections import defaultdict
 
 def filter_by_str(operations: list[dict], search_str: str) -> list:
     """Получение списка словарей, у которых в описании указанная строка."""
+    pattern = r'description\W\W\s\W' + search_str
     filtered_operations = []
     for operation in operations:
-        if re.search(search_str, str(operation)):
+        if re.search(pattern, str(operation)):
             filtered_operations.append(operation)
     return filtered_operations
 
@@ -20,15 +22,15 @@ if __name__ == "__main__":
 
 def filter_by_description(operations: list[dict], description: list) -> dict:
     """Получение соваря категорий и количества операций в каждой категории."""
-    result = {}
+    result = defaultdict(int)
     for item in description:
-        pattern = r'description\W\W\s\W' + item
-        descriptions = re.findall(pattern, str(operations))
-        result[item] = len(descriptions)
+        result[item]
+        counted = Counter(re.findall(item, str(operations)))
+        result.update(counted)
     return result
 
 
 if __name__ == "__main__":
     operations = get_list("operations.json")
-    description = ["Перевод организации", "Перевод со счета на счет"]
+    description = ["Перевод организации", "Перевод со счета на счет", "test"]
     print(filter_by_description(operations, description))
